52149951abd140ff8b0c1d062414aa66a852dc4f
mirxanfozliov <mirxanfozilov@gmail.com>
2021-08-11 14:51:08 +0500

    close bosa yo boladi skidka

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import './topbar.css';
+import { useState } from 'react';
 
 const Topbar = () => {
+
+    const [closedd , setClosedd] = useState(false)
+
     return (
         <div className="topbar">
+            <div className={"box1 " + (closedd && "active")}>
+                <p>Зарегистрируйтесь и получите скидку 20%</p>
+                <span>подробное</span>
+                <div className="closebox" onClick={() => setClosedd(!closedd)}>
+                    <div className="line1"></div>
+                    <div className="line2"></div>
+                </div>
+            </div>
             <div className="box2">
                 <i class="fa fa-map-marker-alt"></i>
             </div>
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -2,12 +2,10 @@ import React from 'react'
 import LogReg from '../components/LogReg';
 import MainCarousel from '../components/MainCarousel';
 import Topbar from '../components/topbar/Topbar';
-import Skidka from '../components/скидка/Skidka';
 
 function MainPage() {
     return (
         <div>
-            <Skidka />
             <Topbar />
             <MainCarousel />
             <LogReg/>
