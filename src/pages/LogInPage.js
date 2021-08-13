51c920582b82fda8d2782d82d43f63a21e2de8aa
salohiddin1221 <salohiddinkhayitov0@gmail.com>
2021-08-13 17:47:05 +0500

    log in start

---
+++ src/pages/LogInPage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function LogInPage() {
+    return (
+        <LogInStyled>
+            
+        </LogInStyled>
+    )
+}
+
+const LogInStyled = styled.div`
+
+`;
+
+export default LogInPage
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -4,12 +4,10 @@ import MainCarousel from '../components/MainCarousel';
 import MainBanner from "../components/MainBanner"
 import Topbar from '../components/topbar/Topbar';
 import NewProducts from '../components/MainProducts/NewProducts';
-<<<<<<< HEAD
 import Footer from '../components/Footer';
-=======
 import MainOffers from '../components/MainOffers/MainOffers';
 import BestSeller from '../components/BestSeller/BestSeller';
->>>>>>> 2f39c4bed9b0df73e4cb08202e44973d5d4c67e9
+
 
 
 
@@ -20,14 +18,10 @@ function MainPage() {
             <MainCarousel />
             <LogReg />
             <MainBanner />
-<<<<<<< HEAD
             <NewProducts/>
-            <Footer/>
-=======
-            <NewProducts />
             <MainOffers />
             <BestSeller /> 
->>>>>>> 2f39c4bed9b0df73e4cb08202e44973d5d4c67e9
+            <Footer/>
         </div>
     )
 }
