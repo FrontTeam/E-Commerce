9a470c3981fd6cdee7cb2c214bd0fa7260ac44ce
Jaloliddin <arabshox007@gmail.com>
2021-08-17 23:30:07 +0500

    responsive qilindi

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,19 +6,20 @@ import logo from "../../images/footerImg/footerlogo-v11.webp";
 import yandexZen from "../../images/footerImg/Yandex_Zen_logo_icon.svg";
 import app from "../../images/footerImg/appstore-banner.png";
 import googlePlay from "../../images/footerImg/googleplay-banner.webp";
-import {
-	faFacebookF,
-	faInstagram,
-	faOdnoklassniki,
-	faVk,
-	faYoutube
-} from "@fortawesome/free-brands-svg-icons";
+import { faFacebookF, faInstagram, faOdnoklassniki, faVk, faYoutube } from "@fortawesome/free-brands-svg-icons";
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 const youtobe = <FontAwesomeIcon icon={faYoutube} />;
 const insta = <FontAwesomeIcon icon={faInstagram} />;
 const ok = <FontAwesomeIcon icon={faOdnoklassniki} />;
 const face = <FontAwesomeIcon icon={faFacebookF} />;
 const vk = <FontAwesomeIcon icon={faVk} />;
+
+
 
 export const Footer = () => {
 	return (
@@ -42,9 +43,7 @@ export const Footer = () => {
 						</div>
 						<div className='Service d-flex pright'>
 							<span>Xizmat</span>
-							<Link to=''>
-								Qanday qilib buyurtma berish kerak
-							</Link>
+							<Link to=''>Qanday qilib buyurtma berish kerak</Link>
 							<Link to=''>Ro'yxatdan o'tish</Link>
 							<Link to=''>To'lov</Link>
 							<Link to=''>Yetkazib berish</Link>
@@ -58,6 +57,58 @@ export const Footer = () => {
 							<Link to=''>Katalogni varaqlash</Link>
 						</div>
 					</div>
+						
+					<div className="footerMenu2">
+						<div className='Company w-310'>
+							<Accordion>
+							<AccordionSummary expandIcon={<ExpandMoreIcon />} >
+								<Typography><span>Kompaniya</span></Typography>
+							</AccordionSummary>
+							<AccordionDetails>
+								<Typography>
+									<li><Link to=''>Faberlic haqida</Link></li>
+									<li><Link to=''>Korporativ qadriyatlar</Link></li>
+									<li><Link to=''>Ilmiy ishlanmalar markazi</Link></li>
+									<li><Link to=''>O'Z Ishlab Chiqarish</Link></li>
+									<li><Link to=''>Yangiliklar</Link></li>
+									<li><Link to=''>Katalog yangiliklari</Link></li>
+								</Typography>
+							</AccordionDetails>
+						</Accordion>
+						</div>
+						<div className='Service w-310'>
+							<Accordion>
+								<AccordionSummary expandIcon={<ExpandMoreIcon />} >
+									<Typography><span>Xizmat</span></Typography>
+								</AccordionSummary>
+								<AccordionDetails>
+									<Typography>
+										<li><Link to=''>Qanday qilib buyurtma berish kerak</Link></li>
+										<li><Link to=''>Ro'yxatdan o'tish</Link></li>
+										<li><Link to=''>To'lov</Link></li>
+										<li><Link to=''>Yetkazib berish</Link></li>
+										<li><Link to=''>Qaytish</Link></li>
+										<li><Link to=''>Savol va Javob</Link></li>
+									</Typography>
+								</AccordionDetails>
+							</Accordion>
+						</div>
+						<div className='Optional w-310'>
+							<Accordion>
+								<AccordionSummary expandIcon={<ExpandMoreIcon />} >
+									<Typography><span>Ixtiyoriy</span></Typography>
+								</AccordionSummary>
+								<AccordionDetails>
+									<Typography>
+										<li><Link to=''>Hamkorlik</Link></li>
+										<li><Link to=''>Topshirish Punktlari</Link></li>
+										<li><Link to=''>Katalogni varaqlash</Link></li>
+									</Typography>
+								</AccordionDetails>
+							</Accordion>
+						</div>
+					</div>
+
 					<div className='footerSocialNetwork'>
 						<span>Ijtimoiy tarmoq</span>
 						<ul>
@@ -104,7 +155,105 @@ export const Footer = () => {
 };
 const FooterWrapper = styled.div`
 	background-color: #fafafa;
+	li{
+		list-style-type: none;
+	}
 	span {
+		font-size: 15px;
+		line-height: 1.125rem;
+		font-weight: 700;
+		margin-bottom: 15px;
+		color: #222;
+	}
+
+	a {
+		font-size: 13px;
+		color: #1c1c1c;
+		text-decoration: none;
+
+		line-height: 1.625rem;
+		&:hover {
+			text-decoration: underline;
+		}
+	}
+
+	.container {
+		padding: 32px 80px 37px;
+	}
+	.footerRow {
+		display: flex;
+		padding-top: 50px;
+	}
+	.footerLogo img {
+		width: 83px;
+	}
+	.footerMenu {
+		display: flex;
+		margin: 0 60px;
+	}
+	.d-flex {
+		display: flex;
+		flex-direction: column;
+	}
+
+	.pright {
+		padding: 0 60px;
+	}
+	.footerMenu2{
+		display: none;
+	}
+	.footerSocialNetwork {
+		padding-left: 30px;
+		ul {
+			display: flex;
+			flex-wrap: wrap;
+			width: 150px;
+			margin-top: 15px;
+
+			li {
+				list-style-type: none;
+				margin: 0 18px 13px 0;
+				overflow: hidden;
+
+				a {
+					font-size: 20px;
+				}
+				img {
+					width: 18px;
+					height: 18px;
+				}
+			}
+		}
+		.footerLaast {
+			display: flex;
+			flex-direction: column;
+			font-size: 13px;
+			.lastImg {
+				margin: 12px 0;
+				overflow: hidden;
+				a {
+					margin: 0 10px;
+				}
+			}
+			p {
+				margin-left: 10px;
+				color: #222;
+			}
+			.lastP {
+				color: #b3b3b3;
+			}
+		}
+	}
+
+
+	//***@media css**//
+
+
+@media only screen and ( min-width: 1025px){
+     .container {
+        width: 100%;
+     }
+	 span {
 		font-size: 15px;
 		line-height: 1.125rem;
 		font-weight: 700;
@@ -187,6 +336,222 @@ const FooterWrapper = styled.div`
 			}
 		}
 	}
+
+
+
+
+
+}
+
+ @media only screen and (min-width:960px) and ( max-width: 1025px){
+     .container {
+        width: 100%;
+		.pright {
+    	padding: 0 25px;
+}
+     }
+    .footerRow{
+        display: block;
+		padding-top: 0;
+		.footerMenu {
+			margin:40px 0;
+			.pright {
+				padding: 0 110px 0 0;
+			}
+		}
+		.footerSocialNetwork {
+			padding-left:0;
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			ul {
+				width: 287px;
+				li{
+					margin-right: 30px;
+				}
+			}
+			p{
+				text-align: center;
+				margin: 15px 0;
+			}
+
+
+
+
+
+	}
+}
+
+
+
+
+} 
+
+@media only screen and (min-width:768px) and ( max-width: 960px){
+	.container {
+			width: 100%;
+	}
+	.footerRow{
+        display: block;
+		padding-top: 0;
+		.footerMenu{
+			margin: 0;
+			.pright{
+				padding:30px 40px 40px 0 ;
+			}
+		}
+		.footerSocialNetwork {
+			padding-left: 0;
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			text-align:center;
+			ul{
+				width: 347px;
+				margin-bottom: 20px;
+				li{
+					margin: 0 20px 13px;
+				}
+			}
+			.lastImg{
+				margin: 30px 0;
+			}
+		}
+	}
+
+}
+
+@media only screen and (min-width:500px) and ( max-width: 768px){
+	.container {
+			width: 100%;
+	}
+	.footerLogo img {
+    	display: none;
+	}
+	.footerRow {
+    display: flex;
+	flex-direction: column;
+    padding-top: 50px;
+
+	.footerMenu{
+		display: none;
+	}
+	.footerMenu2{
+		display: block;
+		margin-left: 8px;
+
+		.MuiPaper-elevation1{
+			box-shadow:none;
+			border-radius: 0;
+			background-color: #fafafa;
+		}
+		.MuiIconButton-root {
+			padding: 0;
+		}
+		.MuiAccordionSummary-root {
+    		min-height: 0;
+		}
+		.MuiIconButton-edgeEnd {
+    		margin-right: 0;
+		}
+		.MuiAccordionSummary-content {
+			margin:0;
+		}
+}
+	}
+	.w-310{
+		width: 300px;
+	}
+	.footerSocialNetwork {
+			padding-left: 0;
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			text-align:center;
+			ul{
+				width: 300px;
+				margin-bottom: 20px;
+				li{
+					margin: 0 15px 10px;
+				}
+			}
+			.footerLaast{
+				.lastImg{
+					margin: 30px 0;
+				}
+			}
+		}
+}
+
+@media only screen and (min-width:360px) and ( max-width: 500px){
+	.container {
+		width: 100%;
+	}
+	.footerLogo img {
+    	display: none;
+	}
+	.footerRow {
+    display: flex;
+	flex-direction: column;
+    padding-top: 50px;
+
+	.footerMenu{
+		display: none;
+	}
+	.footerMenu2{
+		display: block;
+		margin-left: 8px;
+
+		.MuiPaper-elevation1{
+			box-shadow:none;
+			border-radius: 0;
+			background-color: #fafafa;
+		}
+		.MuiIconButton-root {
+			padding: 0;
+		}
+		.MuiAccordionSummary-root {
+    		min-height: 0;
+		}
+		.MuiIconButton-edgeEnd {
+    		margin-right: 0;
+		}
+		.MuiAccordionSummary-content {
+			margin:0;
+		}
+}
+	}
+	.w-310{
+		width: 250px;
+	}
+	.footerSocialNetwork {
+			padding-left: 0;
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			text-align:center;
+			span{
+				margin: 15px 0;
+			}
+			ul{
+				width: 252px;
+				margin-bottom: 20px;
+				li{
+					margin: 0 12px 10px;
+				}
+			}
+			.footerLaast{
+				.lastImg{
+					display: flex;
+					margin: 30px 0;
+				}
+			}
+		}
+
+
+}
+
+
 `;
 
 export default Footer;
